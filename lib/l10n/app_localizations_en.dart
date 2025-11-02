8d961fab5a19a49d2ea994a7c5a9b925f212c91f
DarkNet07 <iparviz50@gmail.com>
2025-11-02 11:41:40 +0300

    app created

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,6 @@
+import 'package:iheartbeat/l10n/app_localizations.dart';
+
+class AppLocalizationsEn extends AppLocalizations {
+  @override
+  String get appDescription => "Your personal fitness and heart rate tracker";
+}
---
+++ lib/l10n/app_localizations_ru.dart
@@ -0,0 +1,7 @@
+import 'package:iheartbeat/l10n/app_localizations.dart';
+
+class AppLocalizationsRu extends AppLocalizations {
+  @override
+  String get appDescription =>
+      "iHeartBeat - Мониторинг здоровья и тренировок";
+}
